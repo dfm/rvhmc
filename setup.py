c56a064aedb133890a3cf57be0ab9d75483e1031
Dan F-M <danfm@nyu.edu>
2018-06-19 10:01:41 -0400

    adding cuda op

--- setup.py
+++ setup.py
@@ -1,19 +1,65 @@
 #!/usr/bin/env python
 
 import os
+import sys
 import tensorflow as tf
 from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+
+
+def customize_compiler_for_nvcc(self):
+    self.src_extensions.append('.cu')
+    default_compiler_so = self.compiler_so
+    super = self._compile
+
+    def _compile(obj, src, ext, cc_args, extra_postargs, pp_opts):
+        if os.path.splitext(src)[1] == '.cu':
+            self.set_executable('compiler_so', "nvcc")
+            postargs = extra_postargs["nvcc"]
+        else:
+            postargs = extra_postargs["gcc"]
+
+        super(obj, src, ext, cc_args, postargs, pp_opts)
+        self.compiler_so = default_compiler_so
+
+    self._compile = _compile
+
+
+class custom_build_ext(build_ext):
+
+    def build_extensions(self):
+        customize_compiler_for_nvcc(self.compiler)
+        build_ext.build_extensions(self)
+
 
 compile_flags = tf.sysconfig.get_compile_flags()
-compile_flags += ["-std=c++11", "-O2", "-mmacosx-version-min=10.9"]
 link_flags = tf.sysconfig.get_link_flags()
+
+sources = [os.path.join("rvhmc", "kepler", "kepler.cc")]
+
+# Check for flag and nvcc
+if "--with-cuda" in sys.argv:
+    index = sys.argv.index("--with-cuda")
+    sys.argv.pop(index)
+    compile_flags += ["-DGOOGLE_CUDA=1"]
+    sources += [os.path.join("rvhmc", "kepler", "kepler.cc.cu")]
+gcc_flags = compile_flags + ["-std=c++11", "-O2", "-march=native", "-fPIC"]
+nvcc_flags = compile_flags + ["-std=c++11", "-shared", "-Xcompiler", "-fPIC",
+                              "-x", "cu",
+                              "--expt-relaxed-constexpr"]
+if sys.platform == "darwin":
+    gcc_flags += ["-mmacosx-version-min=10.9"]
 
 extensions = [
     Extension(
         "rvhmc.kepler_op",
-        sources=[os.path.join("rvhmc", "kepler_op.cc")],
+        sources=sources,
         language="c++",
-        extra_compile_args=compile_flags,
+        include_dirs=[os.path.join("rvhmc", "kepler")],
+        extra_compile_args=dict(
+            nvcc=nvcc_flags,
+            gcc=gcc_flags,
+        ),
         extra_link_args=link_flags,
     ),
 ]
@@ -23,5 +69,6 @@ setup(
     license="MIT",
     packages=["rvhmc"],
     ext_modules=extensions,
+    cmdclass={"build_ext": custom_build_ext},
     zip_safe=True,
 )
